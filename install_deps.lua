7091137500278ade279c04e7af791ea6727b763d
DaApppooo <appo.tcp@gmail.com>
2025-05-23 18:47:44 +0200

    -

--- install_deps.lua
+++ install_deps.lua
@@ -90,7 +90,7 @@ function inst_plutosvg()
     if exists("plutosvg") then
       rm("plutosvg")
     end
-    shell("cd plutosvg && cmake -B build . "..CMAKE_FLAGS.." && cmake --build build "..CMAKE_FLAGS)
+    shell("; cd plutosvg ; cmake -B build . "..CMAKE_FLAGS.." ; cmake --build build "..CMAKE_FLAGS)
     mv("plutosvg/source/*", "include/")
     mv("plutosvg/plutovg/include/plutovg.h", "include/")
     -- mv("plutosvg/build/plutovg/*.a", "include") -- reactivate this line if plutosvg.a is not enough
