6952e60f3e7c89d40ed04a86771a4e264ef734ff
DaApppooo <appo.tcp@gmail.com>
2025-05-24 15:14:36 +0200

    -

--- build.lua
+++ build.lua
@@ -4,7 +4,6 @@ SRC = {"cobz.cpp", "colors.cpp", "img.cpp", "tables.cpp", "shared/utils.cpp",
   "stbiw.cpp", "zip.cpp"
 }
 -- -lplutosvg -lplutovg -lcurl -lz -lzstd -lssl -lpsl -lnghttp2 -lnghttp3 -lngtcp2 -lngtcp2_crypto_quictls -lssh2 -lbrotlidec -lbrotlicommon
-LFLAGS = "-L lib '-Wl,-Bstatic' -lplutosvg -lpluto '-Wl,Bdynamic' -lcurl-x64"
 LD_LIBRARY_PATH = ""
 
 load_os()
@@ -13,16 +12,18 @@ ensure_folder("temp")
 ensure_folder("temp/shared")
 CFLAGS = "-I include -I src/shared"
 if TARGET == "LINUX" then
+  LFLAGS = "-L lib -lplutosvg -lpluto"
   CFLAGS = CFLAGS .. " `pkg-config --cflags libcurl`"
   CFLAGS = CFLAGS .. " -Wl,-rpath," .. LD_LIBRARY_PATH
 elseif TARGET == "WIN" then
+  LFLAGS = "-L lib '-Wl,-Bstatic' -lplutosvg -lpluto '-Wl,Bdynamic' -lcurl-x64"
   if not exists("include/curl") then
     wget("https://curl.se/windows/latest.cgi?p=win64-mingw.zip", "curl.zip")
     extract("curl.zip")
     mv("curl-*-win64-mingw/lib/*", "lib")
     mv("curl-*-win64-mingw/include/*", "include")
     mv("curl-*-win64-mingw/COPYING.txt", "licenses/curl.txt")
-    mv("curl-*-win64-mingw/bin", "licenses/curl.txt")
+    mv("curl-*-win64-mingw/bin/libcurl-x64.dll", "lib")
     rm("curl.zip")
     rm("curl-*-win64-mingw")
   end
